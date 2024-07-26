75a4e2c40c62f22673b6886f8a79badcb90dafe2
dmitsmirnov <dmitsmirnov@bk.ru>
2024-07-26 17:53:42 +0300

    add new files

--- TipKitUIView.swift
+++ TipKitUIView.swift
@@ -15,14 +15,13 @@ struct TipKitUIView: View {
     let addColorTip = AddColorTip()
     let setFavoriteTip = SetFavoriteTip()
     let infoTip = InfoTip()
-    
-    var longPressGameTip = LongPressGameTip()
-    
-    @State var showInfo = false
+    //var longPressGameTip = LongPressGameTip()
+    //@State var showInfo = false
     
     var body: some View {
         
         NavigationStack {
+            
             ScrollView {
                 
                 TipView(setFavoriteTip)
@@ -34,7 +33,7 @@ struct TipKitUIView: View {
                         .frame(height: 100)
                         .contextMenu {
                             Button("Favorite", systemImage: "star") {
-                                
+                                Task { await SetFavoriteTip.setFavotiteEvent.donate() }
                             }
                         }
                 }
@@ -42,21 +41,26 @@ struct TipKitUIView: View {
                 Spacer()
                 
                 Button {
-                    LongPressGameTip.isLoggedIn = true
+                    //LongPressGameTip.isLoggedIn = true
+                    //print(infoTip.status)
+                    //infoTip.invalidate(reason: .displayCountExceeded)
+                    //InfoTip.showTip = true
+                    
+                    InfoTip.showTip = true
+                    //print(infoTip.status)
+                    
+                    //Task { try? Tips.resetDatastore() }
+                    
                 } label: {
                     Image(systemName: "play.circle")
                 }
-                .popoverTip(longPressGameTip)
+                .popoverTip(infoTip, arrowEdge: .top)
                 .padding()
                 
-                Button {
-                    //longPressGameTip.invalidate(reason: .actionPerformed)
-                    //LongPressGameTip.isLoggedIn = false
-                    longPressGameTip.invalidate(reason: .tipClosed)
-                } label: {
-                    Image(systemName: "play.circle")
-                }
                 
+            }
+            .onAppear {
+                Task { await SetFavoriteTip.colorsViewVisitedEcent.donate() }
             }
             .padding()
             .navigationTitle("Colors")
@@ -66,35 +70,22 @@ struct TipKitUIView: View {
                     
                     Button {
                         colors.insert(.random, at: 0)
-                        //
                         addColorTip.invalidate(reason: .actionPerformed)
+                        
                     } label : {
                         Image(systemName: "plus")
                     }
-                    //.popoverTip(addColorTip)
-                    
-//                    Button {
-//                        //showInfo.toggle()
-//                        //LongPressGameTip.isLoggedIn = true
-//                        LongPressGameTip.isLoggedIn = true
-//                        //longPressGameTip.invalidate(reason: .actionPerformed)
-//                        
-//                    } label: {
-//                        Image(systemName: "play.circle")
-//                    }
-                    
-                   
-                    
-                    //.popoverTip(longPressGameTip)
-                    
+//                    .popoverTip(LongPressGameTip_2()) { action in
+//                        guard action.id == "learn-more" else { return } }
+                    .popoverTip(addColorTip)
+                 
                 }
             }
 
         }
-        //            .popoverTip(isPresented: $showInfo, content: InfoTip())
-        //           .popoverTip(infoTip)
-        //
+  
     }
+    
 }
 
 #Preview {
@@ -133,16 +124,38 @@ struct AddColorTip: Tip {
 }
 
 struct SetFavoriteTip: Tip {
+    
+    static let setFavotiteEvent = Event(id: "setFavorite")
+    static let colorsViewVisitedEcent = Event(id: "colorsViewVisited")
+    
+    
     var title: Text {
         Text("Set favorite")
     }
     var message: Text? {
         Text("Tap and hold a color to add it to your favorites")
     }
+    
+    var rules: [Rule] {
+        
+        #Rule(Self.setFavotiteEvent) { event in
+            event.donations.count == 0
+        }
+        
+        #Rule(Self.colorsViewVisitedEcent) { event in
+            event.donations.count > 2
+        }
+        
+    }
 
 }
 
+
 struct InfoTip: Tip {
+    
+    @Parameter
+    static var showTip: Bool = false
+    
     var title: Text {
         Text("Add new color")
             .foregroundStyle(.teal)
@@ -154,11 +167,17 @@ struct InfoTip: Tip {
         Image(systemName: "paintpalette")
     }
     
-    var actions: [Action] {
-        [Action(id: "learn-more", title: "Learn More", perform: {
-            print("'Learn More' pressed")
-        })]
+    var rules: [Rule] {
+        return [
+            #Rule(Self.$showTip) { $0 == true }
+        ]
     }
+    
+//    var actions: [Action] {
+//        [Action(id: "learn-more", title: "Learn More", perform: {
+//            print("'Learn More' pressed")
+//        })]
+//    }
 }
 
 // parameter
@@ -186,20 +205,22 @@ struct LongPressGameTip: Tip {
 // event
 struct LongPressGameTip_2: Tip {
     
-    var title: Text
+    var title: Text {
+        Text("new message")
+    }
     
-    static let appOpenedCount = Event(id: "appOpenedCount")
+    //static let appOpenedCount = Event(id: "appOpenedCount")
      
     //Если вы установили не мгновенную частоту отображения, но у вас есть подсказка, которую вы хотите отобразить немедленно, вы можете сделать это с помощью опции IgnoresDisplayFrequency
     
     // Если подсказка не отменена пользователем вручную, то она будет показана вновь при следующем появлении соответствующего представления даже после запуска приложения. Чтобы избежать повторного показа подсказки пользователю, можно задать значение MaxDisplayCount
-    var options: [TipOption] {
-        [Tip.IgnoresDisplayFrequency(false), Tip.MaxDisplayCount(3)]
-    }
-    
-   
-    var rules: [Rule] {
-        #Rule(Self.appOpenedCount) { $0.donations.count >= 3 }
+//    var options: [TipOption] {
+//        [Tip.IgnoresDisplayFrequency(false), Tip.MaxDisplayCount(3)]
+//    }
+//    
+//   
+//    var rules: [Rule] {
+//        #Rule(Self.appOpenedCount) { $0.donations.count >= 3 }
         
 //        #Rule(Self.appOpenedCount) {
 //            $0.donations.filter {
@@ -207,12 +228,18 @@ struct LongPressGameTip_2: Tip {
 //            }
 //            .count >= 3
 //        }
-        
+//
+//    }
+    
+    var actions: [Action] {
+        [Action(id: "learn-more", title: "Learn More")]
     }
     
     // [...] заголовок, сообщение, ассет, действия и т.д.
     
 }
+
+
 
 // debug
 //Показать все определенные в приложении подсказки
